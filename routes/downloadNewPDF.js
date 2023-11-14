76442e4a35b833548e538a1dc2cad47a9b6c0a62
piyush d <piyushdahiya1218@gmail.com>
2023-11-14 23:45:38 +0530

    added pdf page removal based on user input

--- routes/downloadNewPDF.js
+++ routes/downloadNewPDF.js
@@ -5,11 +5,26 @@ const url = require("url");
 const fs = require("fs");
 const root = require("rootrequire");
 const { PdfDocument } = require("@ironsoftware/ironpdf");
-const {checkboxStates} = require("./getNewPDF");
 
-const transformPDF = async (originalFilePath, fileName) => {
+function convertJsonToArray(totalPages, checkboxStates){
+  const pagesToBeRemoved = [];
+  const checkboxStatesSet = new Set([]);
+  checkboxStates.forEach(state => {
+    if(state!==null && state.isChecked){
+      checkboxStatesSet.add(state.pageNumber-1);
+    }
+  });
+  for(var i=0;i<totalPages;i++){
+    if(!checkboxStatesSet.has(i)){
+      pagesToBeRemoved.push(i);
+    }
+  }
+  return pagesToBeRemoved;
+}
+
+const transformPDF = async (originalFilePath, fileName, pagesToBeRemoved) => {
   const originalFile = await PdfDocument.fromFile(originalFilePath);
-  originalFile.removePage([0, 1]);
+  originalFile.removePage(pagesToBeRemoved);
   const modifiedFolderPath = path.join(root, "public", "modified-files");
   if (!fs.existsSync(modifiedFolderPath)) {
     fs.mkdirSync(modifiedFolderPath);
@@ -19,14 +34,13 @@ const transformPDF = async (originalFilePath, fileName) => {
   return modifiedFilePath;
 };
 
-router.get("/", (req, res) => {
-    console.log(checkboxStates)
-
+router.post("/", function (req, res) {
   const originalFolderPath = path.join(root, "public", "original-files");
-  const originalFilePath = originalFolderPath.concat("/", req.query.fileName);
-  transformPDF(originalFilePath, req.query.fileName)
+  const originalFilePath = originalFolderPath.concat("/", req.body.fileName);
+  const pagesToBeRemoved = convertJsonToArray(req.body.totalPages, req.body.checkboxStates)
+  transformPDF(originalFilePath, req.body.fileName, pagesToBeRemoved)
     .then((modifiedFilePath) => {
-        res.status(200).send(modifiedFilePath);
+      res.status(200).send(modifiedFilePath);
     })
     .catch((error) => {
       console.error(error);
